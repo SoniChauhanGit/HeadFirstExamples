f39cb4129cf5634ee7af1fe214238ff1126e7180
Trisha Gee <trisha.gee@gmail.com>
2021-09-16 16:27:18 +0200

    Initial commit. Code from p531.

---
+++ src/main/java/Jukebox1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Jukebox1 {
+
+    ArrayList<String> songList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+
+    public void go() {
+        Songs s = new Songs();
+        songList = s.getSongs();
+        System.out.println(songList);
+    }
+}
+
+// Below is the "mock" code. A stand in for the actual
+// I/O code that the other programmer will provide later
+
+class Songs {
+    public ArrayList<String> getSongs() {
+        ArrayList<String> songDB = new ArrayList<>();
+        songDB.add("somersault");
+        songDB.add("cassidy");
+        songDB.add("$10");
+        songDB.add("havana");
+        songDB.add("Cassidy");
+        songDB.add("50 Ways");
+        return songDB;
+    }
+}
