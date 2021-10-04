85cbc88fb2843e141d714176eadb1436a3212c75
Trisha Gee <trisha.gee@gmail.com>
2021-10-04 16:03:18 +0200

    A foreach example

---
+++ src/main/java/LambdaForEach.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LambdaForEach {
+
+    MockSongV2File mockFile = new MockSongV2File();
+
+    public static void main(String[] args) {
+        new LambdaForEach().go();
+    }
+
+    public void go() {
+        List<SongV3> mySongs = mockFile.getSongsV3();
+
+        mySongs.forEach(mySong -> System.out.println(mySong));
+
+
+        System.out.println(mySongs);
+        Collections.sort(mySongs);
+        System.out.println(mySongs);
+    }
+}
