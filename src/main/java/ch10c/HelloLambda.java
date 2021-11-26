42aa07c74dfaf83749db98764a4fbdb4710203c9
Trisha Gee <trisha.gee@gmail.com>
2021-11-26 16:57:05 +0100

    Lambdas as behaviour

--- src/main/java/ch10c/HelloLambda.java
+++ src/main/java/ch10c/HelloLambda.java
@@ -1,11 +1,13 @@
 package ch10c;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class HelloLambda {
   public static void main(String[] args) {
-    helloLambda();
+    lambdaAsBehaviour();
   }
 
   static void helloLambda() {
@@ -18,6 +20,17 @@ public class HelloLambda {
                                  .limit(4)
                                  .collect(Collectors.toList());
     System.out.println("result = " + result);
+  }
 
+  static void lambdaAsBehaviour() {
+    List<String> strings = new ArrayList<>(List.of("I", "am", "a", "list", "of", "Strings"));
+    String str = strings.get(2);
+    strings.add("new String");
+    strings.forEach(s -> System.out.println(s));
+
+    List<String> result = strings.stream()
+                                 .sorted((s1, s2) -> s1.compareToIgnoreCase(s2))
+                                 .limit(4)
+                                 .collect(Collectors.toList());
   }
 }
