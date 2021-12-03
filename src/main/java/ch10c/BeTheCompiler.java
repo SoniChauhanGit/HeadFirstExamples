d21b3b9cd10019a6f21b9de5eea8ff8e74f629ba
Trisha Gee <trisha.gee@gmail.com>
2021-12-03 13:10:57 +0100

    Updated be the compiler, includes book version

--- src/main/java/ch10c/BeTheCompiler.java
+++ src/main/java/ch10c/BeTheCompiler.java
@@ -7,19 +7,30 @@ import java.util.function.Supplier;
 public class BeTheCompiler {
   public static void main(String[] args) {
     Runnable runnable = () -> System.out.println("Hi!");
-//    Runnable runnable2 = (String str) -> System.out.println(str);
-
     Consumer<String> consumer = s -> System.out.println(s);
-//    Consumer<String> consumer2 = (str1, str2) -> System.out.println(str1 + ", " + str2);
-//    Consumer<String> consumer3 = s -> "String" + s;
-
-    Function<String, Integer> function = s -> s.length();
-    Function<String, Integer> function2 = (String s) -> s.length();
-//    Function<String, Integer> function3 = (int i) -> "i = " + i;
-    Supplier<String> supplier = () -> "Some string";
-//    Supplier<String> supplier2 = s -> "Some string: " + s;
 //    Supplier<String> supplier3 = () -> System.out.println("Some string");
+//    Consumer<String> consumer2 = (s1, s2) -> System.out.println(s1 + s2);
+//    Runnable runnable2 = (String str) -> System.out.println(str);
+    Function<String, Integer> function = s -> s.length();
+    Supplier<String> supplier = () -> "Some string";
+//    Consumer<String> consumer3 = s -> "String" + s;
+//    Function<String, Integer> function3 = (Integer i) -> "i = " + i;
+//    Supplier<String> supplier2 = s -> "Some string: " + s;
+//    Function<String, Integer> function2 = () -> System.out.println("Some string");
+  }
 
+  void book() {
+//    ❑Runnable r = () -> System.out.println("Hi!");
+//❑Consumer<String> c = s -> System.out.println(s);
+//❑Supplier<String> s = () -> System.out.println("Some string");
+//❑Consumer<String> c = (s1, s2) -> System.out.println(s1 + s2);
+//❑Runnable r = (String str) -> System.out.println(str);
+//❑Function<String, Integer> f = s -> s.length();
+//❑Supplier<String> s = () -> "Some string";
+//❑Consumer<String> c = s -> "String" + s;
+//❑Function<String, Integer> f = (int i) -> "i = " + i;
+//❑Supplier<String> s = s -> "Some string: " + s;
+//❑Function<String, Integer> f = () -> System.out.println("Some string");
 
   }
 }
