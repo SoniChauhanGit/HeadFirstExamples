8fff7571f6047bcc8c24ce1f76d1a00ba7351182
Trisha Gee <trisha.gee@gmail.com>
2021-12-07 13:16:10 +0100

    [Ch10c] Removing BPM.

--- src/main/java/ch10c/Song.java
+++ src/main/java/ch10c/Song.java
@@ -3,15 +3,13 @@ package ch10c;
 public class Song {
   private final String title;
   private final String artist;
-  private final int bpm;
   private final String genre;
   private final int year;
   private final int timesPlayed;
 
-  public Song(String title, String artist, int bpm, String genre, int year, int timesPlayed) {
+  public Song(String title, String artist, String genre, int year, int timesPlayed) {
     this.title = title;
     this.artist = artist;
-    this.bpm = bpm;
     this.genre = genre;
     this.year = year;
     this.timesPlayed = timesPlayed;
@@ -29,10 +27,6 @@ public class Song {
     return artist;
   }
 
-  public int getBpm() {
-    return bpm;
-  }
-
   public String getGenre() {
     return genre;
   }
@@ -46,7 +40,6 @@ public class Song {
     return "Song{" +
             "title='" + title + '\'' +
             ", artist='" + artist + '\'' +
-            ", bpm=" + bpm +
             ", genre='" + genre + '\'' +
             ", year=" + year +
             ", timesPlayed=" + timesPlayed +
