90995626114b7b0de6b806f2b34e8bcb68f3ee70
Trisha Gee <trisha.gee@gmail.com>
2021-12-07 12:59:28 +0100

    [Ch10c] Trying to make sense of the queries we have, and what would be useful for the book

--- src/test/java/ch10c/JukeboxQueriesTest.java
+++ src/test/java/ch10c/JukeboxQueriesTest.java
@@ -24,6 +24,7 @@ class JukeboxQueriesTest {
     System.out.println(allSongs);
   }
 
+  // Sorting, Collect to List
   @Test
   void shouldOrderByLeastPlayed() {
     List<Song> allSongs = songs.getSongs();
@@ -39,20 +40,29 @@ class JukeboxQueriesTest {
   void shouldQueryForGenre() {
     List<Song> allSongs = songs.getSongs();
     // ORDER MATTERS
-    List<String> genres = allSongs.stream().map(song -> song.getGenre())
+    // find all genres in played music
+    List<String> genres = allSongs.stream()
+                                  .map(song -> song.getGenre())
                                   .distinct()
                                   .collect(toList());
     System.out.println("genres = " + genres);
 
-    System.out.println(allSongs.stream().map(song -> song.getGenre())
+    // find number of distinct genres
+    System.out.println(allSongs.stream()
+                               .map(song -> song.getGenre())
                                .distinct()
                                .count());
 
+    // find number songs for each genre
+    // GROUPING
     Map<String, Long> genreCount = allSongs.stream().map(song -> song.getGenre())
                                            .collect(groupingBy(Function.identity(), counting()));
     System.out.println("genreCount = " + genreCount);
   }
 
+  // OPTIONAL
+  // max terminal
+  // Comparator
   @Test
   void shouldFindMostPopularSong() {
     List<Song> allSongs = songs.getSongs();
@@ -82,24 +92,82 @@ class JukeboxQueriesTest {
     System.out.println("result = " + genreCount);
   }
 
+  // STRAIGHTFORWARD
+  // filter
+  // toList
   @Test
   void shouldFindHowManySongsAreSomeSortOfRock() {
     List<Song> allSongs = songs.getSongs();
 
     List<Song> result = allSongs.stream()
-                              .filter(song -> song.getGenre().contains("Rock"))
-                              .collect(toList());
+                                .filter(song -> song.getGenre().contains("Rock"))
+                                .collect(toList());
 
     System.out.println(result);
   }
 
+  // GROUPING
   @Test
   void shouldFindNumberOfTimesSongTitleAppears() {
     List<Song> allSongs = songs.getSongs();
 
     Map<String, Long> result = allSongs.stream()
-                                        .map(Song::getTitle)
-                                        .collect(groupingBy(Function.identity(), counting()));
+                                       .map(Song::getTitle)
+                                       .collect(groupingBy(Function.identity(), counting()));
+
+    System.out.println("result = " + result);
+  }
+
+  // STRAIGHTFORWARD
+  // min terminal
+  // OPTIONAL
+  @Test
+  void shouldGetOldestSong() {
+    List<Song> allSongs = songs.getSongs();
+    Optional<Song> result = allSongs.stream()
+                                    .min(Comparator.comparingInt(Song::getYear));
+
+    System.out.println("result = " + result.get());
+  }
+
+  // STRAIGHTFORWARD
+  // filter (with param)
+  // toList
+  @Test
+  void shouldBeAbleToFindAllVersionsOfASong() {
+    List<Song> allSongs = songs.getSongs();
+
+    String songTitle = "Hurt";
+    List<Song> result = allSongs.stream()
+                                .filter(song -> song.getTitle().equals(songTitle))
+                                .collect(toList());
+
+    System.out.println("result = " + result);
+  }
+
+  @Test
+  void shouldFilterMapCollect() {
+    List<Song> allSongs = songs.getSongs();
+
+    String songTitle = "With a Little Help from My Friends";
+    List<String> result = allSongs.stream()
+                                .filter(song -> song.getTitle().equals(songTitle))
+                                .map(Song::getArtist)
+                                .collect(toList());
+
+    System.out.println("result = " + result);
+  }
+
+  // PRIMITIVE STREAMS
+  @Test
+  void shouldBeAbleToCalculateCountForAllVersionsOfASong() {
+    List<Song> allSongs = songs.getSongs();
+
+    String songTitle = "Hurt";
+    int result = allSongs.stream()
+                         .filter(song -> song.getTitle().equals(songTitle))
+                         .mapToInt(Song::getTimesPlayed)
+                         .sum();
 
     System.out.println("result = " + result);
   }
