c5bd6e6669263a68cb36d96b725f2c0965dbb77f
Trisha Gee <trisha.gee@gmail.com>
2021-12-12 20:10:44 +0100

    [Ch10c] Improving Pool puzzle

--- src/main/java/ch10c/StreamPuzzle.java
+++ src/main/java/ch10c/StreamPuzzle.java
@@ -7,20 +7,32 @@ import java.util.stream.Collectors;
 
 public class StreamPuzzle {
   public static void main(String[] args) {
-    List<Song> songs = new JukeboxData.Songs().getSongs();
+    SongSearch songSearch = new SongSearch();
 
+    songSearch.printTopFiveSongs();
+
+    songSearch.searchForArtist("The Beatles");
+    songSearch.searchForArtist("The Beach Boys");
+  }
+}
+
+class SongSearch {
+  private final List<Song> songs;
+
+  public SongSearch() {
+    songs = new JukeboxData.Songs().getSongs();
+  }
+
+  void printTopFiveSongs() {
     List<String> topFive = songs.stream()
                                 .sorted(Comparator.comparingInt(Song::getTimesPlayed))
                                 .map(song -> song.getTitle())
                                 .limit(5)
                                 .collect(Collectors.toList());
     System.out.println("Top 5 songs: " + topFive);
-
-    searchForArtist(songs, "The Beatles");
-    searchForArtist(songs, "The Beach Boys");
   }
 
-  private static void searchForArtist(List<Song> songs, String artist) {
+  void searchForArtist(String artist) {
     Optional<Song> result = songs.stream()
                                  .filter(song -> song.getArtist().equals(artist))
                                  .findFirst();
