3106f712ac63cd96533dbfa9f63f9d15434c129f
Trisha Gee <trisha.gee@gmail.com>
2021-12-17 11:58:53 +0100

    [Ch15] Replacing first runnable with a lambda.

---
+++ src/main/java/ch15/ThreadTestDrive.java
@@ -0,0 +1,9 @@
+package ch15;
+
+public class ThreadTestDrive {
+  public static void main(String[] args) {
+    Thread myThread = new Thread(() -> System.out.println("top o’ the stack"));
+    myThread.start();
+    System.out.println("back in main");
+  }
+}
