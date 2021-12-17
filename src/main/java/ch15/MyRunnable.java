37d9738b205ddbecd9e44291cf773570c20867ed
Trisha Gee <trisha.gee@gmail.com>
2021-12-17 11:57:29 +0100

    [Ch15] 2nd Ed First runnable

---
+++ src/main/java/ch15/MyRunnable.java
@@ -0,0 +1,24 @@
+package ch15;
+
+public class MyRunnable implements Runnable {
+  public void run() {
+    go();
+  }
+
+  public void go() {
+    doMore();
+  }
+
+  public void doMore() {
+    System.out.println("top o’ the stack");
+  }
+}
+
+class ThreadTestDrive {
+  public static void main(String[] args) {
+    Runnable threadJob = new MyRunnable();
+    Thread myThread = new Thread(threadJob);
+    myThread.start();
+    System.out.println("back in main");
+  }
+}
