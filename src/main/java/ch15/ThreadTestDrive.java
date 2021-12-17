7509ba98eebadd5368ca87d6234527b6e27b8100
Trisha Gee <trisha.gee@gmail.com>
2021-12-17 18:15:11 +0100

    [Ch15] Update first thread test drive to use the executor service instead. It's generally fairly consistent in its order, but at least it prints stuff out "out of order"

--- src/main/java/ch15/ThreadTestDrive.java
+++
@@ -1,9 +0,0 @@
-package ch15;
-
-public class ThreadTestDrive {
-  public static void main(String[] args) {
-    Thread myThread = new Thread(() -> System.out.println("top o’ the stack"));
-    myThread.start();
-    System.out.println("back in main");
-  }
-}
