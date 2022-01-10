b514c9f817d805e11fc4761c27806580de3b0836
Trisha Gee <trisha.gee@gmail.com>
2022-01-10 15:50:12 +0100

    [Ch15] Optimise imports

--- src/main/java/ch15/DailyAdviceServer.java
+++ src/main/java/ch15/DailyAdviceServer.java
@@ -1,10 +1,7 @@
 package ch15;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
