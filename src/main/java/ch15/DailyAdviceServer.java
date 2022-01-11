adada4d2ada7cf72cbda9a247e7526c394196b55
Trisha Gee <trisha.gee@gmail.com>
2022-01-11 11:30:39 +0100

    [Ch15] DailyAdviceServer formatting.

--- src/main/java/ch15/DailyAdviceServer.java
+++ src/main/java/ch15/DailyAdviceServer.java
@@ -7,11 +7,13 @@ import java.net.Socket;
 import java.util.Random;
 
 public class DailyAdviceServer {
-  final private String[] adviceList = {"Take smaller bites",
+  final private String[] adviceList = {
+          "Take smaller bites",
           "Go for the tight jeans. No they do NOT make you look fat.",
           "One word: inappropriate",
           "Just for today, be honest. Tell your boss what you *really* think",
           "You might want to rethink that haircut."};
+  private final Random randomGenerator = new Random();
 
   public static void main(String[] args) {
     DailyAdviceServer server = new DailyAdviceServer();
@@ -37,7 +39,6 @@ public class DailyAdviceServer {
   }
 
   private String getAdvice() {
-    Random randomGenerator = new Random();
     return adviceList[randomGenerator.nextInt(adviceList.length)];
   }
 }
