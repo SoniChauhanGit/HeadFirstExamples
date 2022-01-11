1e2d9ed0700da8afe0d3cdeb1c01c55c3abb6623
Trisha Gee <trisha.gee@gmail.com>
2022-01-11 11:23:17 +0100

    [Ch15] DailyAdviceClient uses SocketChannels and try with resources.

--- src/main/java/ch15/DailyAdviceClient.java
+++ src/main/java/ch15/DailyAdviceClient.java
@@ -2,29 +2,24 @@ package ch15;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 
 public class DailyAdviceClient {
-
   public static void main(String[] args) {
     DailyAdviceClient client = new DailyAdviceClient();
     client.go();
   }
 
   public void go() {
-    try {
-      Socket socket = new Socket("127.0.0.1", 4242);
-
-      InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
-      BufferedReader reader = new BufferedReader(streamReader);
-
+    try (SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 4242))) {
+      BufferedReader reader = new BufferedReader(Channels.newReader(socketChannel, StandardCharsets.UTF_8));
       String advice = reader.readLine();
       System.out.println("Today you should: " + advice);
-
-      reader.close();
-    } catch(IOException ex) {
-      ex.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
