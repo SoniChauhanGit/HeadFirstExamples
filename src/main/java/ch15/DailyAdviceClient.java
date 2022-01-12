32b88860dee3fd69c9617ccb6f4d3c5bb240c4f7
Trisha Gee <trisha.gee@gmail.com>
2022-01-12 13:24:23 +0100

    [Ch15] DailyAdviceClient as it appears in the book.

--- src/main/java/ch15/DailyAdviceClient.java
+++ src/main/java/ch15/DailyAdviceClient.java
@@ -2,24 +2,27 @@ package ch15;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 
 public class DailyAdviceClient {
-  public static void main(String[] args) {
-    DailyAdviceClient client = new DailyAdviceClient();
-    client.go();
-  }
-
   public void go() {
-    try (SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 4242))) {
-      BufferedReader reader = new BufferedReader(Channels.newReader(socketChannel, StandardCharsets.UTF_8));
+    InetSocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 4242);
+    try (SocketChannel socketChannel = SocketChannel.open(serverAddress)) {
+      Reader channelReader = Channels.newReader(socketChannel, StandardCharsets.UTF_8);
+      BufferedReader reader = new BufferedReader(channelReader);
       String advice = reader.readLine();
       System.out.println("Today you should: " + advice);
+      reader.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public static void main(String[] args) {
+    new DailyAdviceClient().go();
   }
 }
