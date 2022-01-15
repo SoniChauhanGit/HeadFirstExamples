4876fa103048e8cf0c9e1ce0dbe99b50cd688319
Trisha Gee <trisha.gee@gmail.com>
2022-01-15 19:56:53 +0100

    [Ch15] Simplified the test harness for unpredictable run order

--- src/main/java/ch15/UnpredictableRunOrder.java
+++ src/main/java/ch15/UnpredictableRunOrder.java
@@ -4,37 +4,27 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class UnpredictableRunOrder {
-
-}
-
-class RunWithThread {
-  public static void main (String[] args) {
-    for (int i = 0; i < 20; i++) {
-      runThread();
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+      ExecutorTestDrive.main(args);
     }
   }
+}
 
-  private static void runThread() {
+class ThreadTestDrive {
+  public static void main (String[] args) {
     Thread myThread = new Thread(() -> System.out.println("top o’ the stack"));
     myThread.start();
     System.out.println("back in main");
   }
 }
 
-class RunWithExecutor {
+class ExecutorTestDrive {
   public static void main (String[] args) {
     ExecutorService executor = Executors.newSingleThreadExecutor();
-    for (int i = 0; i < 100; i++) {
-      executeRunnable(executor);
-    }
-
-    executor.shutdown();
-  }
-
-  private static void executeRunnable(ExecutorService executor) {
     executor.execute(() -> System.out.println("top o’ the stack"));
-
     System.out.println("back in main");
+    executor.shutdown();
   }
 }
 
