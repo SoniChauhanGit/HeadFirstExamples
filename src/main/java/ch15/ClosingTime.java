96486c1edf8eb4df137b643cd8d01ea5ac68d37d
Trisha Gee <trisha.gee@gmail.com>
2022-01-17 12:06:05 +0100

    [Ch15] Replacing methods with Runnables for our jobs

--- src/main/java/ch15/ClosingTime.java
+++ src/main/java/ch15/ClosingTime.java
@@ -12,10 +12,10 @@ public class ClosingTime {
 
   static void shutdownGracefully() {
     ExecutorService threadPool = Executors.newFixedThreadPool(2);
-    threadPool.execute(() -> longJob("Long Job 1"));
-    threadPool.execute(() -> shortJob("Short Job"));
-    threadPool.execute(() -> shortJob("Long Job 2"));
-    threadPool.execute(() -> shortJob("Shouldn't start job"));
+    threadPool.execute(new LongJob("Long Job 1"));
+    threadPool.execute(new ShortJob("Short Job"));
+    threadPool.execute(new ShortJob("Long Job 2"));
+    threadPool.execute(new ShortJob("Shouldn't start job"));
     threadPool.shutdown();
     try {
       System.out.println("Finished? " + threadPool.awaitTermination(1, TimeUnit.SECONDS));
@@ -26,64 +26,54 @@ public class ClosingTime {
 
   static void forceShutdown() {
     ExecutorService threadPool = Executors.newFixedThreadPool(2);
-    threadPool.execute(() -> longJob("Long Job 1"));
-    threadPool.execute(() -> shortJob("Short Job"));
-    threadPool.execute(() -> longJob("Long Job 2"));
-    threadPool.execute(() -> shortJob("Shouldn't start job"));
+    threadPool.execute(new LongJob("Long Job 1"));
+    threadPool.execute(new ShortJob("Short Job"));
+    threadPool.execute(new LongJob("Long Job 2"));
+    threadPool.execute(new ShortJob("Shouldn't start job"));
     List<Runnable> unfinished = threadPool.shutdownNow();
     System.out.println("unfinished.size() = " + unfinished.size());
     for (Runnable runnable : unfinished) {
-      System.out.println(runnable);
+      System.out.println(((NamedJob) runnable).getJobName());
     }
-//    try {
-//      System.out.println("Finished? " + threadPool.awaitTermination(1, TimeUnit.SECONDS));
-//    } catch (InterruptedException e) {
-//      e.printStackTrace();
-//    }
   }
 
-  private static void longJob(String jobName) {
-    // use sleep() to pretend like this is a long-running job
+}
+
+abstract class NamedJob implements Runnable {
+  protected String jobName;
+
+  NamedJob(String jobName) {
+    this.jobName = jobName;
+  }
+
+  public String getJobName() {
+    return jobName;
+  }
+}
+
+
+class ShortJob extends NamedJob {
+  ShortJob(String jobName) {
+    super(jobName);
+  }
+
+  public void run() {
+    System.out.println(jobName);
+  }
+}
+
+class LongJob extends NamedJob {
+  LongJob(String jobName) {
+    super(jobName);
+  }
+
+  public void run() {
     try {
       TimeUnit.SECONDS.sleep(3);
       System.out.println(jobName);
     } catch (InterruptedException e) {
+      System.out.println("Job interrupted: " + jobName);
       e.printStackTrace();
     }
   }
-
-  private static void shortJob(String jobName) {
-    System.out.println(jobName);
-  }
-
-
-  class ShortJob implements Runnable {
-    private String jobName;
-
-    ShortJob(String jobName) {
-      this.jobName = jobName;
-    }
-
-    public void run() {
-      System.out.println(jobName);
-    }
-  }
-
-  class LongJob implements Runnable {
-    private String jobName;
-
-    LongJob(String jobName) {
-      this.jobName = jobName;
-    }
-
-    public void run() {
-      try {
-        TimeUnit.SECONDS.sleep(3);
-        System.out.println(jobName);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
 }
