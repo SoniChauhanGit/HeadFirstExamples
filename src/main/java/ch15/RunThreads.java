e7f0150195f067ed5e71485b06a847e7017dc8a4
Trisha Gee <trisha.gee@gmail.com>
2022-01-17 11:26:03 +0100

    [Ch15] Version of the RunThreads code for the book

--- src/main/java/ch15/RunThreads.java
+++ src/main/java/ch15/RunThreads.java
@@ -7,15 +7,15 @@ public class RunThreads {
 
   public static void main(String[] args) {
     ExecutorService threadPool = Executors.newFixedThreadPool(2);
-    threadPool.execute(() -> run("Job 1"));
-    threadPool.execute(() -> run("Job 2"));
+    threadPool.execute(() -> runJob("Job 1"));
+    threadPool.execute(() -> runJob("Job 2"));
     threadPool.shutdown();
   }
 
-  public static void run(String jobName) {
+  public static void runJob(String jobName) {
     for (int i = 0; i < 25; i++) {
       String threadName = Thread.currentThread().getName();
-      System.out.println(jobName + " is running on thread " + threadName);
+      System.out.println(jobName + " is running on " + threadName);
     }
   }
 }
