be56d377b8abfa56d78d2115c6211bb582c45edf
Trisha Gee <trisha.gee@gmail.com>
2022-01-17 17:48:01 +0100

    [Ch15] Book example for race condition/lost update

--- src/main/java/ch15/LostUpdate.java
+++ src/main/java/ch15/LostUpdate.java
@@ -4,22 +4,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class TestSyncTest {
+public class LostUpdate {
   public static void main(String[] args) throws InterruptedException {
-    ExecutorService pool = Executors.newFixedThreadPool(6);
+    ExecutorService pool = Executors.newFixedThreadPool(30);
     Balance balance = new Balance();
     for (int i = 0; i < 1000; i++) {
-      pool.execute(() -> incrementBalance(balance));
+      pool.execute(() -> balance.increment());
     }
     pool.shutdown();
     // make sure the pool has finished running all the updates before printing the output
     if (pool.awaitTermination(1, TimeUnit.MINUTES)) {
       System.out.println("balance = " + balance.balance);
     }
-  }
-
-  private static void incrementBalance(Balance balance) {
-      balance.increment();
   }
 }
 
