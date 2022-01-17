24cf64c629c476ff11daa320d75bf554682237e1
Trisha Gee <trisha.gee@gmail.com>
2022-01-17 15:29:26 +0100

    [Ch15] Renamed Ryan and Monica test

--- src/main/java/ch15/RyanAndMonicaTest.java
+++ src/main/java/ch15/RyanAndMonicaTest.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class TestRyanAndMonica {
+public class RyanAndMonicaTest {
   public static void main(String[] args) {
     BankAccount account = new BankAccount();
     RyanAndMonicaJob ryan = new RyanAndMonicaJob("Ryan", account);
