9ce01376f8a979b475b70f37273d492d532c4f37
Trisha Gee <trisha.gee@gmail.com>
2022-01-31 18:17:27 +0100

    [Ch10b] Cleaning up and reordering mountains exercise

--- src/main/java/ch10b/SortMountains.java
+++ src/main/java/ch10b/SortMountains.java
@@ -1,46 +1,51 @@
 package ch10b;
 
 import java.util.*;
+
 public class SortMountains {
-  LinkedList<Mountain> mtn = new LinkedList<Mountain>();
-  class NameCompare implements Comparator <Mountain> {
-    public int compare(Mountain one, Mountain two) {
-      return one.name.compareTo(two.name);
-    }
-  }
-  class HeightCompare implements Comparator <Mountain> {
-    public int compare(Mountain one, Mountain two) {
-      return (two.height - one.height);
-    }
-  }
-  public static void main(String [] args) {
+
+  public static void main(String[] args) {
     new SortMountains().go();
   }
+
   public void go() {
-    mtn.add(new Mountain("Longs", 14255));
-    mtn.add(new Mountain("Elbert", 14433));
-    mtn.add(new Mountain("Maroon", 14156));
-    mtn.add(new Mountain("Castle", 14265));
-    System.out.println("as entered:\n" + mtn);
+    List<Mountain> mountains = new LinkedList<>();
+    mountains.add(new Mountain("Longs", 14255));
+    mountains.add(new Mountain("Elbert", 14433));
+    mountains.add(new Mountain("Maroon", 14156));
+    mountains.add(new Mountain("Castle", 14265));
+    System.out.println("as entered:\n" + mountains);
     NameCompare nc = new NameCompare();
-    Collections.sort(mtn, nc);
-    System.out.println("by name:\n" + mtn); 
+    mountains.sort(nc);
+    System.out.println("by name:\n" + mountains);
     HeightCompare hc = new HeightCompare();
-    Collections.sort(mtn, hc);
-    System.out.println("by height:\n" + mtn); 
+    mountains.sort(hc);
+    System.out.println("by height:\n" + mountains);
   }
 }
-class Mountain {
-  String name;
-  int height;
 
-  Mountain(String n, int h) {
-    name = n;
-    height = h;
+class NameCompare implements Comparator<Mountain> {
+  public int compare(Mountain one, Mountain two) {
+    return one.name.compareTo(two.name);
+  }
+}
+
+class HeightCompare implements Comparator<Mountain> {
+  public int compare(Mountain one, Mountain two) {
+    return (two.height - one.height);
+  }
+}
+
+class Mountain {
+  final String name;
+  final int height;
+
+  Mountain(String name, int height) {
+    this.name = name;
+    this.height = height;
   }
 
   public String toString() {
     return name + " " + height;
   }
-
 }
