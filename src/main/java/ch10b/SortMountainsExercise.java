8b399169ad6ec481576161b1442a5e7ccc4cbbde
Trisha Gee <trisha.gee@gmail.com>
2022-01-31 16:01:20 +0100

    [Ch10b] Mountains exercise for 10b

---
+++ src/main/java/ch10b/SortMountainsExercise.java
@@ -0,0 +1,44 @@
+package ch10b;
+
+//import __________________;
+public class SortMountainsExercise {
+//  LinkedList______________ mtn = new LinkedList____________();
+//  class NameCompare ___________________________________ {
+//    public int compare(Mountain one, Mountain two) {
+//      return ___________________________;
+//    }
+//  }
+//  class HeightCompare _______________________________ {
+//    public int compare(Mountain one, Mountain two) {
+//      return (__________________________);
+//    }
+//  }
+//  public static void main(String [] args) {
+//    new SortMountains().go();
+//  }
+//  public void go() {
+//    mtn.add(new Mountain(“Longs”, 14255));
+//    mtn.add(new Mountain(“Elbert”, 14433));
+//    mtn.add(new Mountain(“Maroon”, 14156));
+//    mtn.add(new Mountain(“Castle”, 14265));
+//    System.out.println(“as entered:\n” + mtn);
+//    NameCompare nc = new NameCompare();
+//    _________________________________;
+//    System.out.println(“by name:\n” + mtn);
+//    HeightCompare hc = new HeightCompare();
+//    _________________________________;
+//    System.out.println(“by height:\n” + mtn);
+//  }
+//}
+//class Mountain {
+//  __________________;
+//  _________________;
+//
+//  ______________________ {
+//    _________________;
+//    _________________;
+//  }
+//  _________________________ {
+//    ______________________________;
+//  }
+}
