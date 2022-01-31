38ba0eb301e7d0b63d3f84b7139536745f8f6a87
Trisha Gee <trisha.gee@gmail.com>
2022-01-31 10:48:10 +0100

    [AppA] Use star imports

--- src/main/java/appA/MusicServer.java
+++ src/main/java/appA/MusicServer.java
@@ -1,14 +1,9 @@
 package appA;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class MusicServer {
   final List<ObjectOutputStream> clientOutputStreams = new ArrayList<>();
