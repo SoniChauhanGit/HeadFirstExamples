abe6fa853f4468c56e94b39fd88b9f52928c0973
Trisha Gee <trisha.gee@gmail.com>
2022-02-01 11:37:57 +0100

    [Ch10b] Formatting for TestTree

--- src/main/java/ch10b/TestTree.java
+++ src/main/java/ch10b/TestTree.java
@@ -3,21 +3,21 @@ package ch10b;
 import java.util.*;
 
 public class TestTree {
-   public static void main (String[] args) {
-      new TestTree().go();
-   }
+  public static void main(String[] args) {
+    new TestTree().go();
+  }
 
-   public void go() {
-     Book b1 = new Book("How Cats Work");
-     Book b2 = new Book("Remix your Body");
-     Book b3 = new Book("Finding Emo");
-     
-     TreeSet<Book> tree = new TreeSet<>();
-     tree.add(b1);
-     tree.add(b2);
-     tree.add(b3);
-     System.out.println(tree);
-   }
+  public void go() {
+    Book b1 = new Book("How Cats Work");
+    Book b2 = new Book("Remix your Body");
+    Book b3 = new Book("Finding Emo");
+
+    TreeSet<Book> tree = new TreeSet<>();
+    tree.add(b1);
+    tree.add(b2);
+    tree.add(b3);
+    System.out.println(tree);
+  }
 }
 
 class Book implements Comparable<Book> {
@@ -25,6 +25,7 @@ class Book implements Comparable<Book> {
   public Book(String t) {
     title = t;
   }
+
   public int compareTo(Book other) {
     return title.compareTo(other.title);
   }
