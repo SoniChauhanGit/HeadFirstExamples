0a66610bc3a0823be9feee10e86503c114418c18
Trisha Gee <trisha.gee@gmail.com>
2022-02-01 18:22:15 +0100

    [Ch10b] Simplifying the lambdas version of Jukebox

--- src/main/java/ch10b/Jukebox6.java
+++ src/main/java/ch10b/Jukebox6.java
@@ -1,32 +1,19 @@
 package ch10b;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Jukebox6 {
-  List<SongV3> songList = new ArrayList<SongV3>();
-  MockSongV3File mockFile = new MockSongV3File();
-
   public static void main(String[] args) {
     new Jukebox6().go();
   }
 
   public void go() {
-    songList = mockFile.getSongsV3();
+    List<SongV3> songList = MockSongs.getSongsV3();
     System.out.println(songList);
+
     songList.sort((one, two) -> one.getTitle().compareTo(two.getTitle()));
     System.out.println(songList);
-    songList.sort((one, two) -> one.getArtist().compareTo(two.getArtist()));
-    System.out.println(songList);
-  }
 
-  public void ch10c() {
-    songList = mockFile.getSongsV3();
-    System.out.println(songList);
-    songList.sort((one, two) -> {
-      return one.getTitle().compareTo(two.getTitle());
-    });
-    System.out.println(songList);
     songList.sort((one, two) -> one.getArtist().compareTo(two.getArtist()));
     System.out.println(songList);
   }
