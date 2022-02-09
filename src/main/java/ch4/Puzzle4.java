63a707b99f208fe974410d7c858872ee7624547d
Trisha Gee <trisha.gee@gmail.com>
2022-02-09 12:20:03 +0100

    [Ch4] Renaming exercise version of Puzzle4

--- src/main/java/ch4/Puzzle4.java
+++ src/main/java/ch4/Puzzle4.java
@@ -38,17 +38,18 @@ class Value {
 //public class Puzzle4 {
 //  public static void main(String [] args) {
 //    ___________________________________
-//    int y = 1;
-//    int x = 0;
-//    int result = 0;
-//    while (x < 6) {
+//    int number = 1;
+//    int i = 0;
+//    while (i < 6) {
 //      ___________________________
 //      ___________________________
-//      y = y * 10;
+//      number = number * 10;
 //      _________________
 //    }
-//    x = 6;
-//    while (x > 0) {
+//
+//    int result = 0;
+//    i = 6;
+//    while (i > 0) {
 //      _________________
 //      result = result + ___________________
 //    }
@@ -57,13 +58,13 @@ class Value {
 //}
 //
 //class ___________ {
-//  int ivar;
+//  int intValue;
 //  ________  ______ doStuff(int _________) {
-//    if (ivar > 100) {
+//    if (intValue > 100) {
 //      return _________________________
 //    } else {
 //      return _________________________
 //    }
 //  }
 //}
-//
+
