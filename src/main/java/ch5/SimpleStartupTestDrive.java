1b2f487f48ffa269a5c05f7aa5c16af114ae4f63
Trisha Gee <trisha.gee@gmail.com>
2022-02-09 17:06:03 +0100

    [Ch5] Test code updated to the code on p 103

--- src/main/java/ch5/SimpleStartupTestDrive.java
+++ src/main/java/ch5/SimpleStartupTestDrive.java
@@ -3,11 +3,17 @@ package ch5;
 public class SimpleStartupTestDrive {
   public static void main(String[] args) {
     SimpleStartup dot = new SimpleStartup();
-    int[] locations = {2, 3, 4};
+
+    int[] locations = {2,3,4};
     dot.setLocationCells(locations);
+
     int userGuess = 2;
     String result = dot.checkYourself(userGuess);
-    System.out.println(result);
+    String testResult = "failed";
+    if (result.equals("hit") ) {
+      testResult = "passed";
+    }
+    System.out.println(testResult);
   }
 }
 
