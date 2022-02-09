8ed40b9f690dbc4e776bbc968e3de6d3fbd6887c
Trisha Gee <trisha.gee@gmail.com>
2022-02-09 11:18:04 +0100

    [Ch4] Puzzle 4

---
+++ src/main/java/ch4/Puzzle4.java
@@ -0,0 +1,67 @@
+package ch4;
+
+public class Puzzle4 {
+  public static void main(String [] args) {
+    ___________________________________
+    int y = 1;
+    int x = 0;
+    int result = 0;
+    while (x < 6) {
+      ___________________________
+      ___________________________
+      y = y * 10;
+      _________________
+    }
+    x = 6;
+    while (x > 0) {
+      _________________
+      result = result + ___________________
+    }
+    System.out.println("result " + result);
+  }
+}
+
+class ___________ {
+  int ivar;
+  ________  ______ doStuff(int _________) {
+    if (ivar > 100) {
+      return _________________________
+    } else {
+      return _________________________
+    }
+  }
+}
+
+
+//public class Puzzle4 {
+//  public static void main(String [] args) {
+//    ___________________________________
+//    int y = 1;
+//    int x = 0;
+//    int result = 0;
+//    while (x < 6) {
+//      ___________________________
+//      ___________________________
+//      y = y * 10;
+//      _________________
+//    }
+//    x = 6;
+//    while (x > 0) {
+//      _________________
+//      result = result + ___________________
+//    }
+//    System.out.println("result " + result);
+//  }
+//}
+//
+//class ___________ {
+//  int ivar;
+//  ________  ______ doStuff(int _________) {
+//    if (ivar > 100) {
+//      return _________________________
+//    } else {
+//      return _________________________
+//    }
+//  }
+//}
+//
