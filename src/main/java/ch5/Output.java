7983f95da5c4faefb2981214ed0f3112fa4c0136
Trisha Gee <trisha.gee@gmail.com>
2022-02-09 17:51:57 +0100

    [Ch5] Renaming and reformatting be the JVM

--- src/main/java/ch5/Output.java
+++ src/main/java/ch5/Output.java
@@ -1,21 +1,20 @@
 package ch5;
 
 class Output {
-
-  public static void main(String [] args) {
-    Output o = new Output();
-    o.go();
+  public static void main(String[] args) {
+    Output output = new Output();
+    output.go();
   }
 
   void go() {
-    int y = 7;
-    for(int x = 1; x < 8; x++) {
-      y++;
-      if (x > 4) {
-        System.out.print(++y + " ");
+    int value = 7;
+    for (int i = 1; i < 8; i++) {
+      value++;
+      if (i > 4) {
+        System.out.print(++value + " ");
       }
-      if (y > 14) {
-        System.out.println(" x = " + x);
+      if (value > 14) {
+        System.out.println(" i = " + i);
         break;
       }
     }
