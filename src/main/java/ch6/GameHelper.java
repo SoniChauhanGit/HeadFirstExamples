f71a6a6fd1c19f68a9626d27bef621ececd7e7ed
Trisha Gee <trisha.gee@gmail.com>
2022-02-18 13:59:23 +0100

    [Ch6] GameHelper: Comments and some code organisation

--- src/main/java/ch6/GameHelper.java
+++ src/main/java/ch6/GameHelper.java
@@ -38,17 +38,17 @@ public class GameHelper {
       int location = random.nextInt(GRID_SIZE);         // get random starting point
 
       for (int i = 0; i < startupCoords.length; i++) {  // create an array of proposed co-ordinates for the startup
-        startupCoords[i] = location;
+        startupCoords[i] = location;                    // put current location in the array
         location += alignment.getIncrement();           // calculate the next location
       }
       System.out.println("Trying: " + Arrays.toString(startupCoords));
 
-      if (startupFits(startupCoords, alignment)) {        // check the startup would fit on the grid
-        success = coordsAvailable(startupCoords);         // if it fits, check those locations aren't taken
-      }                                                   // end loop
-    }                                                     // end while
+      if (startupFits(startupCoords, alignment)) {      // check the startup would fit on the grid
+        success = coordsAvailable(startupCoords);       // if it fits, check those locations aren't already taken
+      }                                                 // end loop
+    }                                                   // end while
 
-    savePositionToGrid(startupCoords);
+    savePositionToGrid(startupCoords);                  // these coords passed everything, so update the grid with this startup
     ArrayList<String> alphaCells = convertCoordsToAlphaFormat(startupCoords);
     System.out.println(alphaCells);
     System.out.println(this);
@@ -64,28 +64,6 @@ public class GameHelper {
     }
   }
 
-  private ArrayList<String> convertCoordsToAlphaFormat(int[] startupCoords) {
-    ArrayList<String> alphaCells = new ArrayList<String>();
-    for (int index : startupCoords) {                 // for each grid coordinate
-      alphaCells.add(getAlphaCoordsFromIndex(index)); // turn it into an "a0" style and add to a list
-    }
-    return alphaCells;
-  }
-
-  void savePositionToGrid(int[] startupCoords) {
-    for (int index : startupCoords) {                 
-      grid[index] = 1;                         // mark master grid position as 'used'
-    }
-  }
-
-  String getAlphaCoordsFromIndex(int index) {
-    int row = calculateRowFromIndex(index);           // get row value
-    int column = index % GRID_LENGTH;        // get numeric column value
-
-    String letter = ALPHABET.substring(column, column + 1); // convert column index to letter
-    return letter + row;
-  }
-
   boolean coordsAvailable(int[] startupCoords) {
     for (int coord : startupCoords) {   // check all potential positions
       if (grid[coord] != 0) {           // this co-ordinate already has an entry in the grid
@@ -94,6 +72,28 @@ public class GameHelper {
       }
     }
     return true;                        // if the method got this far, there were no clashes, yay!
+  }
+
+  void savePositionToGrid(int[] startupCoords) {
+    for (int index : startupCoords) {
+      grid[index] = 1;                         // mark master grid position as 'used'
+    }
+  }
+
+  private ArrayList<String> convertCoordsToAlphaFormat(int[] startupCoords) {
+    ArrayList<String> alphaCells = new ArrayList<String>();
+    for (int index : startupCoords) {                 // for each grid coordinate
+      alphaCells.add(getAlphaCoordsFromIndex(index)); // turn it into an "a0" style and add to a list
+    }
+    return alphaCells;
+  }
+
+  String getAlphaCoordsFromIndex(int index) {
+    int row = calculateRowFromIndex(index);           // get row value
+    int column = index % GRID_LENGTH;        // get numeric column value
+
+    String letter = ALPHABET.substring(column, column + 1); // convert column index to letter
+    return letter + row;
   }
 
   private Alignment getAlignment() {
