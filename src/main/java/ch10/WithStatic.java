3dc0fed8e14965867befd8e68b5561a5f3293939
Trisha Gee <trisha.gee@gmail.com>
2022-02-24 16:57:27 +0100

    [Ch10] Fixed static imports example

--- src/main/java/ch10/WithStatic.java
+++ src/main/java/ch10/WithStatic.java
@@ -1,20 +1,11 @@
 package ch10;
 
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
-import static java.lang.Math.sqrt;
-import static java.lang.Math.tan;
+import static java.lang.Math.*;
 import static java.lang.System.out;
 
 class WithStatic {
   public static void main(String[] args) {
     out.println("sqrt " + sqrt(2.0));
     out.println("tan " + tan(60));
-  }
-
-  void otherStaticImports() {
-    Stream.of("string")
-          .filter(Predicate.not(String::isEmpty));
   }
 }
