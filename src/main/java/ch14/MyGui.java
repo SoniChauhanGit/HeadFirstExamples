a3ee7ea7d8eaeda21deab51ed6d1782eabcdb8f1
Trisha Gee <trisha.gee@gmail.com>
2022-02-26 13:48:43 +0100

    [Ch14] Single ActionListener with if statement, nasty.

--- src/main/java/ch14/MyGui.java
+++ src/main/java/ch14/MyGui.java
@@ -4,15 +4,27 @@ import javax.swing.*;
 import java.awt.event.*;
 
 class MyGui implements ActionListener {
+  private JButton colorButton;
+  private JButton labelButton;
   private JFrame frame;
   private JLabel label;
-  // lots of code here and then:
+  // declare a bunch of instance variables here
 
-  public void actionPerformed(ActionEvent event) {
-    frame.repaint();
+  public void go() {
+    // build gui
+    colorButton = new JButton();
+    labelButton = new JButton();
+    colorButton.addActionListener(this);
+    labelButton.addActionListener(this);
+    // more gui code here ...
   }
 
-//  public void actionPerformed(ActionEvent event) {
-//    label.setText("That hurt!");
-//  }
+  public void actionPerformed(ActionEvent event) {
+    if (event.getSource() == colorButton) {
+      frame.repaint();
+    } else {
+      label.setText("That hurt!");
+    }
+  }
 }
+
