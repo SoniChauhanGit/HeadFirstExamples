fe39ef482a3104eeb79ffe3abe263a387d1e1201
Trisha Gee <trisha.gee@gmail.com>
2022-02-26 17:40:48 +0100

    [Ch14] Updating sharpen exercise

--- src/main/java/ch14/btc/InnerButton.java
+++ src/main/java/ch14/btc/InnerButton.java
@@ -1,11 +1,11 @@
 package ch14.btc;
 
 import javax.swing.*;
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.*;
 
 class InnerButton {
-  private JFrame frame;
-  private JButton b;
+  private JButton button;
 
   public static void main(String[] args) {
     InnerButton gui = new InnerButton();
@@ -13,26 +13,26 @@ class InnerButton {
   }
 
   public void go() {
-    frame = new JFrame();
+    JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(
             JFrame.EXIT_ON_CLOSE);
 
-    b = new JButton("A");
-//    b.addActionListener();
+    button = new JButton("A");
+    button.addActionListener(new ButtonListener());
 
     frame.getContentPane().add(
-            BorderLayout.SOUTH, b);
+            BorderLayout.SOUTH, button);
     frame.setSize(200, 100);
     frame.setVisible(true);
   }
 
-//  class BListener extends ActionListener {
-//    public void actionPerformed(ActionEvent e) {
-//      if (b.getText().equals("A")) {
-//        b.setText("B");
-//      } else {
-//        b.setText("A");
-//      }
-//    }
-//  }
+  class ButtonListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      if (button.getText().equals("A")) {
+        button.setText("B");
+      } else {
+        button.setText("A");
+      }
+    }
+  }
 }
