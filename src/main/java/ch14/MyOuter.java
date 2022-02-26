d39b44da61e78c7401331895552a31ec8b63d104
Trisha Gee <trisha.gee@gmail.com>
2022-02-26 08:14:58 +0100

    [Ch14] Main code classes from book

---
+++ src/main/java/ch14/MyOuter.java
@@ -0,0 +1,20 @@
+package ch14;
+
+class MyOuter  {
+
+   private int x;
+
+   MyInner inner = new MyInner();
+
+   public void doStuff() {
+      inner.go();
+    }
+
+   
+   class MyInner {
+      void go() {
+         x = 42;
+       }
+    } // close inner class
+
+} // close outer class
