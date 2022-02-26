37fe38a61346b0213ff3a0551342f6a358add6d5
Trisha Gee <trisha.gee@gmail.com>
2022-02-26 21:52:04 +0100

    [Ch15] Add two buttons to the panel

--- src/main/java/ch15/Panel1.java
+++ src/main/java/ch15/Panel1.java
@@ -10,17 +10,19 @@ public class Panel1 {
     gui.go();
   }
 
-
   public void go() {
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
     panel.setBackground(Color.darkGray);
 
     JButton button = new JButton("shock me");
+    JButton buttonTwo = new JButton("bliss");
+
     panel.add(button);
+    panel.add(buttonTwo);
 
     frame.getContentPane().add(BorderLayout.EAST, panel);
-    frame.setSize(200, 200);
+    frame.setSize(250,200);
     frame.setVisible(true);
   }
 
