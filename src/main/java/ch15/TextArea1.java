1335d8b33db76010750fe3efac0df49a19ce2c70
Trisha Gee <trisha.gee@gmail.com>
2022-02-27 08:42:08 +0100

    [Ch15] Examples for swing components

--- src/main/java/ch15/TextArea1.java
+++ src/main/java/ch15/TextArea1.java
@@ -4,24 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
-public class TextArea1 implements ActionListener {
-
-  JTextArea text;
-
+public class TextArea1 {
   public static void main(String[] args) {
     TextArea1 gui = new TextArea1();
     gui.go();
   }
 
-
   public void go() {
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
+
     JButton button = new JButton("Just Click It");
-    button.addActionListener(this);
-    text = new JTextArea(10, 20);
+
+    JTextArea text = new JTextArea(10, 20);
     text.setLineWrap(true);
+    button.addActionListener(e -> text.append("button clicked "));
 
     JScrollPane scroller = new JScrollPane(text);
     scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -34,10 +31,6 @@ public class TextArea1 implements ActionListener {
 
     frame.setSize(350, 300);
     frame.setVisible(true);
-  }
-
-  public void actionPerformed(ActionEvent ev) {
-    text.append("button clicked \n ");
   }
 }
       
