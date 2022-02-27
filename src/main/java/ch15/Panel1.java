bc3cb8d4266cfc7266b4cbafc5fa818ca43c350b
Trisha Gee <trisha.gee@gmail.com>
2022-02-27 07:54:27 +0100

    [Ch15] Add box layout to the panel

--- src/main/java/ch15/Panel1.java
+++ src/main/java/ch15/Panel1.java
@@ -15,12 +15,12 @@ public class Panel1 {
     JPanel panel = new JPanel();
     panel.setBackground(Color.darkGray);
 
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
     JButton button = new JButton("shock me");
     JButton buttonTwo = new JButton("bliss");
-
     panel.add(button);
     panel.add(buttonTwo);
-
     frame.getContentPane().add(BorderLayout.EAST, panel);
     frame.setSize(250,200);
     frame.setVisible(true);
