a99da721c18223522306e3b740b2de84f290f0fa
Trisha Gee <trisha.gee@gmail.com>
2022-02-27 12:47:30 +0100

    [Ch16] Initial code from book

---
+++ src/main/java/ch16/QuizCard.java
@@ -0,0 +1,15 @@
+package ch16;
+
+public class QuizCard {
+  public QuizCard(String text, String text1) {
+
+  }
+
+  public int getQuestion() {
+    return 0;
+  }
+
+  public int getAnswer() {
+    return 0;
+  }
+}
