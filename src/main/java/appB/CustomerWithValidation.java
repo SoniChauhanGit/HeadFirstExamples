2cbb75b58f5d85374babc81ca74732cac5d5b82c
Trisha Gee <trisha.gee@gmail.com>
2022-03-11 13:33:30 +0100

    [appB] Compact constructor

--- src/main/java/appB/CustomerWithValidation.java
+++ src/main/java/appB/CustomerWithValidation.java
@@ -1,16 +1,13 @@
 package appB;
 
 public record CustomerWithValidation(int id, String name) {
-  public CustomerWithValidation(int id, String name) {
+  public CustomerWithValidation {
     if (id < 0) {
       throw new ValidationException();
     }
-    this.id = id;
-    this.name = name;
   }
 
   public int id() {
-    checkEverythingIsOk();
     return id;
   }
 
